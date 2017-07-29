c8a3af5251441b6b6192f007bf2a29d047fe3884
awctomlinson <mexander@gmail.com>
2017-07-28 17:06:55 -0700

    working on threshing

--- pyandor/gui/pyandorGUI.py
+++ pyandor/gui/pyandorGUI.py
@@ -26,6 +26,8 @@ class Frame(QtGui.QWidget):
         self.overlay_image = None
         self.overlay_active = False
         self.overlay_opacity = 0.5
+        self.overlay_threshold = 128
+        self.do_threshold = False
 
         # init widgets
         self.image_viewer = ImageWidget(self)
@@ -33,13 +35,15 @@ class Frame(QtGui.QWidget):
         # top level layout
         layout_frame = QtGui.QVBoxLayout()
 
-        # layout for overlay transparency
-        layout_slider = self.setup_slider_opacity()
+        # layout for overlay threshold and opacity
+        layout_slider_threshold = self.setup_slider_threshold()
+        layout_slider_opacity = self.setup_slider_opacity()
 
-        # layout slider next to image viewer
+        # layout sliders on either side of image viewer
         layout_viewer_slider = QtGui.QHBoxLayout()
+        layout_viewer_slider.addLayout(layout_slider_threshold)
         layout_viewer_slider.addWidget(self.image_viewer)
-        layout_viewer_slider.addLayout(layout_slider)
+        layout_viewer_slider.addLayout(layout_slider_opacity)
 
         # layout with controls along bottom
         layout_controls = self.setup_controls()
@@ -71,6 +75,9 @@ class Frame(QtGui.QWidget):
         # setup controls
         control_splitter = QtGui.QHBoxLayout()
 
+        self.checkbox_threshold = QtGui.QCheckBox('Threshold')
+        self.checkbox_threshold.stateChanged.connect(self.on_checkbox_threshold)
+
         self.button_capture_overlay = QtGui.QPushButton('Capture Overlay')
         self.button_capture_overlay.clicked.connect(self.on_button_capture_overlay)
 
@@ -87,12 +94,36 @@ class Frame(QtGui.QWidget):
         self.spinbox_exposure.setDecimals(1)
         self.spinbox_exposure.valueChanged.connect(self.on_spinbox_exposure)
 
+        control_splitter.addWidget(self.checkbox_threshold)
         control_splitter.addWidget(self.button_capture_overlay)
         control_splitter.addWidget(self.button_overlay)
         control_splitter.addWidget(self.button_start_pause)
         control_splitter.addWidget(self.spinbox_exposure)
 
         return control_splitter
+
+    def setup_slider_threshold(self):
+        """
+        Sets up the slider for opacity
+
+        :return: layout with slider and opacity
+        """
+        self.slider_overlay_threshold = QtGui.QSlider(QtCore.Qt.Vertical)
+        self.slider_overlay_threshold.setMinimum(0)
+        self.slider_overlay_threshold.setMaximum(255)
+        self.slider_overlay_threshold.setValue(128)
+        self.slider_overlay_threshold.setTickPosition(QtGui.QSlider.TicksRight)
+        self.slider_overlay_threshold.setTickInterval(64)
+        self.slider_overlay_threshold.valueChanged.connect(self.on_slider_overlay_threshold)
+
+        value = self.overlay_threshold
+        self.label_slider_overlay_threshold = QtGui.QLabel('{}'.format(value))  # TODO: fix resize at 100%
+
+        layout_slider_label = QtGui.QVBoxLayout()
+        layout_slider_label.addWidget(self.slider_overlay_threshold)
+        layout_slider_label.addWidget(self.label_slider_overlay_threshold)
+
+        return layout_slider_label
 
     def setup_slider_opacity(self):
         """
@@ -131,24 +162,53 @@ class Frame(QtGui.QWidget):
 
         :param img_data_overlay: image data of the overlay
         """
-        self.image_viewer.update_overlay(img_data_overlay, overlay_opacity=self.overlay_opacity)
+        if self.do_threshold:
+            threshed = np.copy(img_data_overlay)
+            threshed[threshed < self.overlay_threshold] = 0
+
+            threshed = np.dstack([threshed] * 3)
+            # alpha = np.zeros((threshed.shape[1], threshed.shape[0], 1))
+            # threshed = np.concatenate([threshed, alpha], axis=2)
+            print(threshed[20][101])
+
+            self.image_viewer.update_overlay(threshed, overlay_opacity=self.overlay_opacity)
+
+        else:
+            self.image_viewer.update_overlay(img_data_overlay, overlay_opacity=self.overlay_opacity)
+
+    def on_checkbox_threshold(self, state):
+        """
+        Updates whether or not to threshold overlay
+        """
+        checked = state == QtCore.Qt.Checked
+
+        self.do_threshold = checked
+
+        if self.overlay_active:
+            self.update_overlay(self.overlay_image)
 
     def on_button_capture_overlay(self):
         """
         Captures the current image to display as overlay.
         """
-        self.overlay_image = self.image_viewer.viewer.image
+        # self.overlay_image = self.image_viewer.viewer.image
+
+        # capture image as qimage
+        # qimage = self.image_viewer.viewer.qimage
+        # w, h = qimage.width(), qimage.height()
+        #
+        # ptr = qimage.bits()
+        # ptr.setsize(qimage.byteCount())
+        # arr = np.array(ptr).reshape(h, w, 4)  # Copies the data
+        # self.overlay_image = np.fliplr(np.rot90(arr, -1))
+
+        data = self.image_viewer.viewer.image
+        min, max = data.min(), data.max()
+        self.overlay_image = pg.functions.rescaleData(data, 255. / (max - min), min, dtype=np.uint8)
 
         # TODO: decide on whether or not to refresh overlay on click
         if self.overlay_active:
             self.update_overlay(self.overlay_image)
-
-        q = self.image_viewer.viewer.qimage
-        w, h = q.width(), q.height()
-
-        ptr = q.bits()
-        ptr.setsize(q.byteCount())
-        arr = np.array(ptr).reshape(h, w, 4)  # Copies the data
 
     def on_button_overlay(self):
         """
@@ -211,6 +271,18 @@ class Frame(QtGui.QWidget):
         if self.overlay_active:
             self.update_overlay(self.overlay_image)
 
+    def on_slider_overlay_threshold(self):
+        """
+        Changes the opacity of the overlay
+        """
+        value = self.slider_overlay_threshold.value()
+        self.overlay_threshold = value
+
+        self.label_slider_overlay_threshold.setText('{}'.format(value))
+
+        if self.overlay_active:
+            self.update_overlay(self.overlay_image)
+
     def closeEvent(self, event):
         """
         Intercept close event to properly shut down camera and thread.
@@ -250,14 +322,17 @@ class ImageWidget(pg.GraphicsLayoutWidget, object):
         """
         self.viewer.setImage(img_data, autoLevels=True)
 
-    def update_overlay(self, img_data_overlay, overlay_opacity):
+    def update_overlay(self, img_data_overlay, overlay_opacity=None):
         """
         Updates overlay data
 
         :param img_data_overlay: image data of overlay
         :param overlay_opacity: transparency of the overlay
         """
-        self.viewer_overlay.setImage(img_data_overlay, autoLevels=True, opacity=overlay_opacity)
+        if overlay_opacity is not None:
+            self.viewer_overlay.setImage(img_data_overlay, autoLevels=True, opacity=overlay_opacity)
+        else:
+            self.viewer_overlay.setImage(img_data_overlay, autoLevels=True)
 
 
 if __name__ == '__main__':
